10380ae7f22de85682c2fde59cffcc3cf397635f
thriguna <thrigu@gmail.com>
2024-09-30 13:33:51 +0530

    Intial commit

--- app.py.py
+++ app.py.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(_name_)
-
+ 
 @app.route('/')
 def hello_world():
-    return 'Hello, Jenkins!'
+    return  'Hello, Jenkins!'
 
-if _name_ == '_main_':
+if _name== 'main_':
     app.run(host='0.0.0.0', port=5000)
